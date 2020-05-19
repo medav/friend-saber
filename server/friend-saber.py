9ab12bc834815713b8455b8e37d26bd2426394ee
Michael Davies <michaelstoby@gmail.com>
2020-05-18 22:14:54 -0500

    Make ScoreList a class

---
+++ server/friend-saber.py
@@ -0,0 +1,34 @@
+import json
+import discord
+import sys
+
+from songlist import SongList
+
+client = discord.Client()
+cfg_filename = sys.argv[1]
+
+with open(cfg_filename) as f:
+    config = json.load(f)
+
+lists = [
+    SongList(client, sl)
+    for sl in config['song-lists']
+]
+
+
+@client.event
+async def on_ready():
+    for l in lists:
+        await l.OnReady()
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+
+    for l in lists:
+        if l.ShouldClaim(msg):
+            await l.OnMessage(msg)
+            break
+
+client.run(config['api-key'])
--- server/songlist.py
+++ server/songlist.py
@@ -1,135 +1,116 @@
-import json
 import discord
-import sys
 
-from utils import *
-from beatsaber import *
+class SongList():
+    def __init__(self, client, config):
+        self.client = client
+        self.config = config
+        self.approved = {}
+        self.pending = {}
 
-client = discord.Client()
-cfg_filename = sys.argv[1]
+    def Debug(self, msg):
+        print('SongList ["{}"]: {}'.format(self.config['channel'], msg))
 
-with open(cfg_filename) as f:
-    config = json.load(f)
-
-def CreateListEntry(song_code):
-    return '!bsr {} {}'.format(song_code, GetSongUrl(song_code))
-
-def IsListEntry(msg):
-    return msg.author == client.user and IsSongRequest(msg.content)
-
-def CreatePendEntry(song_code):
-    return '!pend {} {}'.format(song_code, GetSongUrl(song_code))
-
-def IsPendEntry(msg):
-    return msg.author == client.user and msg.content.startswith('!pend')
-
-async def AddSong(msg, song_code):
-    global channel
-    global approved
-
-    print('Adding {} to list'.format(song_code))
-
-    await msg.delete()
-    approved[song_code] = await channel.send(CreateListEntry(song_code))
-
-async def PendSong(msg, song_code):
-    global channel
-    global approved
-
-    print('Pending {}'.format(song_code))
-
-    await msg.delete()
-    pending[song_code] = await channel.send(CreatePendEntry(song_code))
-
-def IsListMod(user):
-    return user in mod_role
-
-@client.event
-async def on_ready():
-    global channel
-    global mod_role
-    global approved
-    global pending
-
-    print('We have logged in as {0.user}'.format(client))
-
-    channel = discord.utils.get(
-        client.get_all_channels(),
-        name=config['channel'])
-
-    mod_role = set(discord.utils.get(
-        channel.guild.roles,
-        name='list-mod').members)
-
-    print('Mods: {}'.format(mod_role))
-
-    approved = {}
-    pending = {}
-
-    async for msg in channel.history(limit=10000):
-        if IsListEntry(msg):
-            approved[GetSongCode(msg.content)] = msg
-
-    async for msg in channel.history(limit=10000):
-        if IsPendEntry(msg):
-            pending[GetSongCode(msg.content)] = msg
-
-    print('Built song cache! Here is what I found:')
-    for song_code in approved:
-        print('In List: {}'.format(song_code))
-
-    for song_code in pending:
-        print('Pending: {}'.format(song_code))
-
-@client.event
-async def on_message(msg):
-    global channel
-    global mod_role
-    global approved
-    global pending
-
-    sender = msg.author
-    if sender == client.user:
-        return
-
-    if msg.channel != channel:
-        return
-
-    text = msg.content
-
-    if text == '!update_roles':
-        mod_role = set(discord.utils.get(
-            channel.guild.roles,
+    def ReloadRoles(self):
+        self.mod_role = set(discord.utils.get(
+            self.channel.guild.roles,
             name='list-mod').members)
+        self.Debug('mod_role: {}'.format(self.mod_role))
 
-        print('mod_role:', mod_role)
-        await msg.delete()
+    def IsListMod(self, user):
+        return user in self.mod_role
 
-    elif IsSongRequest(text):
-        song_code = GetSongCode(text)
+    async def OnReady(self):
+        self.Debug('Starting...')
 
-        if song_code in approved or song_code in pending:
-            await msg.delete()
+        self.channel = discord.utils.get(
+            self.client.get_all_channels(),
+            name=self.config['channel'])
+
+        self.ReloadRoles()
+
+        async for msg in self.channel.history(limit=10000):
+            if self.IsListEntry(msg):
+                self.approved[SongList.GetSongCode(msg.content)] = msg
+
+        async for msg in self.channel.history(limit=10000):
+            if self.IsPendEntry(msg):
+                self.pending[SongList.GetSongCode(msg.content)] = msg
+
+    @staticmethod
+    def IsSongRequest(s):
+        return s.startswith('!bsr')
+
+    @staticmethod
+    def GetSongCode(req):
+        return req.split()[1].strip()
+
+    @staticmethod
+    def GetSongUrl(song_code):
+        return 'https://beatsaver.com/beatmap/{}'.format(song_code)
+
+    @staticmethod
+    def CreateListEntry(song_code):
+        return '!bsr {} {}'.format(song_code, SongList.GetSongUrl(song_code))
+
+    @staticmethod
+    def CreatePendEntry(song_code):
+        return '!pend {} {}'.format(song_code, SongList.GetSongUrl(song_code))
+
+    def IsListEntry(self, msg):
+        return msg.author == self.client.user and \
+            SongList.IsSongRequest(msg.content)
+
+    def IsPendEntry(self, msg):
+        return msg.author == self.client.user and \
+            msg.content.startswith('!pend')
+
+    async def AddSong(self, msg, song_code):
+        self.Debug('Adding {} to list'.format(song_code))
+        self.approved[song_code] = \
+            await self.channel.send(SongList.CreateListEntry(song_code))
+
+    async def PendSong(self, msg, song_code):
+        self.Debug('Pending {}'.format(song_code))
+        self.pending[song_code] = \
+            await self.channel.send(SongList.CreatePendEntry(song_code))
+
+    def ShouldClaim(self, msg):
+        return msg.channel == self.channel
+
+    async def OnMessage(self, msg):
+        sender = msg.author
+        text = msg.content
+
+        if text == '!update_roles':
+            self.ReloadRoles()
+
+        elif SongList.IsSongRequest(text):
+            song_code = SongList.GetSongCode(text)
+
+            if song_code not in self.approved and song_code not in self.pending:
+                if self.IsListMod(sender):
+                    await self.AddSong(msg, song_code)
+                else:
+                    await self.PendSong(msg, song_code)
+
+        elif text.startswith('!approve'):
+            song_code = SongList.GetSongCode(text)
+
+            if song_code in self.pending:
+                old_text = self.pending[song_code].content
+                await self.pending[song_code].edit(
+                    content=old_text.replace('!pend', '!bsr'))
+                self.approved[song_code] = self.pending[song_code]
+                del self.pending[song_code]
+
+        elif text.startswith('!deny'):
+            song_code = SongList.GetSongCode(text)
+
+            if song_code in self.pending:
+                await self.pending[song_code].delete()
+                del self.pending[song_code]
 
         else:
-            if IsListMod(sender):
-                await AddSong(msg, song_code)
-            else:
-                await PendSong(msg, song_code)
+            self.Debug('Deleting message')
 
-    elif text.startswith('!approve'):
         await msg.delete()
-        song_code = GetSongCode(text)
-
-        if song_code in pending:
-            old_text = pending[song_code].content
-            await pending[song_code].edit(content=old_text.replace('!pend', '!bsr'))
-            approved[song_code] = pending[song_code]
-            del pending[song_code]
-
-    else:
-        print('Deleting message')
-        await msg.delete()
-
-client.run(config['api-key'])
-
